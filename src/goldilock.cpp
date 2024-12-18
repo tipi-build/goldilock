853a7d308ed737e1aa3d972c16d13741085cf46f
Lambourl <luc@tipi.build>
2024-12-18 12:18:13 +0100

    🔧 remove macos specific

--- src/goldilock.cpp
+++ src/goldilock.cpp
@@ -254,7 +254,7 @@ namespace tipi::goldilock
         }
       }
 
-      #if BOOST_OS_LINUX  || (BOOST_OS_MACOS && BOOST_ARCH_AARCH64)
+      #if BOOST_OS_LINUX
       signal(SIGCHLD, SIG_IGN); // we don't want to handle child signals from our deteched goldilock to avoid them handing around as zombies
       #endif
 
