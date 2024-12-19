99cf0d18360c9692f519be4f6f19d47360c539e7
Lambourl <luc@tipi.build>
2024-12-19 10:12:39 +0100

    🔧 remove cancel and clear to search what fail on ci

--- environments/macos-clang-cxx17.cmake
+++ environments/macos-clang-cxx17.cmake
@@ -2,21 +2,3 @@ include_guard()
 
 include(${CMAKE_CURRENT_LIST_DIR}/compiler/clang.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/flags/cxx17.cmake)
-
-add_compile_options(
-  $<$<COMPILE_LANGUAGE:C,CXX>:-fsanitize=address>)
-add_link_options(
-  $<$<COMPILE_LANGUAGE:C,CXX>:-fsanitize=address>
-  $<$<COMPILE_LANGUAGE:C,CXX>:-static-libsan>)
-add_compile_options(
-  $<$<COMPILE_LANGUAGE:C,CXX>:-Wno-uninitialized>)
-
-add_compile_options(
-  $<$<COMPILE_LANGUAGE:C,CXX>:-Wno-string-concatenation>)
-add_compile_options(
-  $<$<COMPILE_LANGUAGE:C,CXX>:-Wno-shift-overflow>)
-add_compile_options(
-  $<$<COMPILE_LANGUAGE:C,CXX>:-Wno-misleading-indentation>)
-
-
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address")
