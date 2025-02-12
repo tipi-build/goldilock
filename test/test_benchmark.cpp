5fe369024d8c39bc35f9cce96b638939209ae426
Yannic Staudt <yannic@tipi.build>
2025-02-12 15:18:41 +0100

    Improved benchmark test to avoid blocking for a long time on reading a future in case of failures

--- test/test_benchmark.cpp
+++ test/test_benchmark.cpp
@@ -35,6 +35,22 @@ namespace goldilock::test {
 
   using namespace std::literals;
 
+
+  struct task_wrapper {
+    size_t task_index;
+    bool started;
+    bp::child child_process;
+
+    template <class... Param>
+    task_wrapper(size_t ix, boost::asio::io_context& io_context, Param &&... cmd) 
+      : task_index{ix}
+      , started{false}
+      , child_process{io_context, cmd..., bp::extend::on_success=[this](auto & exec) { started = true; }}
+    {
+      //
+    }
+  };
+
   BOOST_AUTO_TEST_CASE(goldilocked_write_many_instances) {
 
     auto wd = get_goldilock_case_working_dir();
@@ -74,19 +90,33 @@ namespace goldilock::test {
     }
     
     std::chrono::steady_clock::time_point bench_start = std::chrono::steady_clock::now();
-    const size_t tasks_expected = std::thread::hardware_concurrency() * 4; // 4 exe's per core...
+    
+    // a small not to future self and elsewhom will be reading this 
+    // this concurrency test not only tests if many goldilock instances will
+    // be able to work through the queue, but does this in an interlocking
+    // fashion because each goldilock tries to acquire two distinct lockfile
+    // so there's a theoretical (nCores * mult) * (nCores * mult) combinatorial of sequences
+    // that might have to be tried in the very worst case. 
+    //
+    // So setting mult = 4 as below, is quite a lot - independently of which
+    // machine we're looking at    
+    const size_t tasks_expected = std::thread::hardware_concurrency() * 4;
+    
     std::atomic_size_t tasks_done = 0;
+    std::atomic_size_t tasks_failed = 0;
+    bool master_lock_released = false;
 
-    std::vector<bp::child> child_processes;
+    std::vector<std::shared_ptr<task_wrapper>> tasks;
 
-    // submit 100 goldilocks...
-    for(size_t task_ix = 0; task_ix < tasks_expected; task_ix++) {
+    size_t task_ix = 0;
+    size_t task_creation_failed = 0;
+    while(task_ix < tasks_expected) {
 
-      auto fut_ptr = std::make_shared<std::future<std::string>>();
-      child_processes.emplace_back(
-        host_goldilock_executable_path(), "--lockfile", gl_A_lockfile, "--lockfile", gl_B_lockfile, "--", support_app_append_to_file_bin, "-s", std::to_string(task_ix) + ":"s, "-n", "5", "-f", write_output_dest.generic_string(), "-i", "1" ,      
-        ios, bp::start_dir=wd, (bp::std_out & bp::std_err) > *fut_ptr, bp::std_in < bp::null,
-        bp::on_exit=[task_ix=task_ix, &tasks_done, bench_start, &fut_ptr](int exit_code, std::error_code const& ec) {
+      std::cout << "Creating task " << task_ix << std::endl;
+      auto task = std::make_shared<task_wrapper>(task_ix, ios, 
+        host_goldilock_executable_path(), "--lockfile", gl_A_lockfile, "--lockfile", gl_B_lockfile, "--", support_app_append_to_file_bin, "-s", std::to_string(task_ix) + ":"s, "-n", "5", "-f", write_output_dest.generic_string(), "-i", "1" ,
+        bp::start_dir=wd, bp::std_out > stdout, bp::std_err > stderr, bp::std_in < bp::null,
+        bp::on_exit=[task_ix=task_ix, &tasks_done, &tasks_failed, &bench_start, &master_lock_released](int exit_code, std::error_code const& ec) {
           std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
           std::cout << "Task " << task_ix << " returned with code: " << exit_code << " (after " << std::chrono::duration_cast<std::chrono::milliseconds>(now - bench_start).count() << "ms)" << std::endl;
           
@@ -94,54 +124,86 @@ namespace goldilock::test {
             std::cout << "Error code = " << ec.message() << std::endl;
           }
 
-          if(exit_code != 0) {
-            std::cout << "----------\n" << fut_ptr->get() << "\n-----------" << std::endl;
+          if(!master_lock_released) {
+            std::cout << "Task finished before master lock was released!" << std::endl;
           }
-          
+
           tasks_done++;
+          
+          if(exit_code != 0) {
+            tasks_failed++;
+            BOOST_REQUIRE(exit_code == 0);
+          }
         }        
       );
 
-      // don't start them too quickly to not overwhelm the os
-      std::cout << "Starting task " << task_ix << std::endl;
-      std::this_thread::sleep_for(100ms);
-    }
+      std::cout << " - waiting to for task " << task_ix << " to start" << std::endl;
 
+      size_t wait_iteration = 0;
+      while(!task->started && task->child_process.running()) {
+        std::cout << "." << std::to_string(task->started) << std::ends;
+        std::this_thread::sleep_for(10ms);
+
+        if(wait_iteration++ % 50 == 0) { /* every half a second or so */
+          std::cout << "." << std::to_string(task->started) << std::ends;
+        }        
+      }
+
+      if(task->started && task->child_process.running()) {
+        std::cout << " - SUCCESS" << std::endl;
+        task_ix++;
+        tasks.push_back(task);
+      }
+      else {
+        std::cout << " - FAILED" << std::endl;
+        BOOST_REQUIRE(task_creation_failed++ < 10); // should not fail too often...
+      }
+    }
+    
     // now's the real starting point....
     bench_start = std::chrono::steady_clock::now();
 
     std::cout << "Releasing master lock" << std::endl;
+    master_lock_released = true;
     tipi::goldilock::file::touch_file_permissive(master_unlockfile);
-    std::chrono::steady_clock::time_point bench_expiry = std::chrono::steady_clock::now() + 180s;
+    
+    std::chrono::steady_clock::time_point ts_master_lock_released = std::chrono::steady_clock::now();
+    std::chrono::steady_clock::time_point bench_expiry = ts_master_lock_released + 180s;
 
     std::cout << "Running for 180s..." << std::endl;
 
     while(std::chrono::steady_clock::now() < bench_expiry && tasks_done < tasks_expected) {
-      std::cout << "Tasks completed: " << tasks_done << " out of " << tasks_expected << std::endl;
+      std::cout << "(status) tasks completed: " << tasks_done << " out of " << tasks_expected << std::endl;
       std::this_thread::sleep_for(1s);
     }
 
     BOOST_ASSERT(tasks_done == tasks_expected);
 
-    for(auto& chld : child_processes) {
-      if(chld.running()) {
-        chld.terminate();
+    std::chrono::steady_clock::time_point ts_all_tasks_finished = std::chrono::steady_clock::now();
+    auto all_tasks_duration = std::chrono::duration_cast<std::chrono::milliseconds>(ts_all_tasks_finished - ts_master_lock_released).count();
+    std::cout << "All tasks completed in: " << all_tasks_duration << "ms" << std::endl;
+    std::cout << "Average time per tasks: " << (all_tasks_duration / tasks_expected)  << "ms" << std::endl;
+
+    for(auto& chld : tasks) {
+      if(chld->child_process.running()) {
+        chld->child_process.terminate();
       }
     }
 
+    BOOST_ASSERT(tasks_failed == 0);
+
+
     auto file_content = tipi::goldilock::file::read_file_content(write_output_dest);
-    std::cout << "Testing goldilocked multilogfile write output:\n-------------\n" << file_content << "\n-------------\nExpecting no mixing of entries" << std::endl;
+    std::cout << "Testing goldilocked write output:\n-------------\n" << file_content << "\n-------------\nExpecting no mixing of entries" << std::endl;
 
     for(size_t task_ix = 0; task_ix < tasks_expected; task_ix++) {
       BOOST_REQUIRE(boost::regex_search(file_content, boost::regex{"(("s + std::to_string(task_ix) + ":){5})"s})); // search for "<task_ix>:<task_ix>:<task_ix>:<task_ix>:<task_ix>:"
     }
 
-    child_processes.clear();
+    tasks.clear();
     ios.stop();
     threads.join_all();
     t_master.join();
-
-
 
   }
 }
