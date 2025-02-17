592ae7f14dda7a99850305300b7a48b28ff34d63
Yannic Staudt <yannic@tipi.build>
2025-02-17 11:24:34 +0100

    :wrench: added tests for --timeout and --no-timeout

---
+++ include/goldilock/goldilock_constants.hpp
@@ -0,0 +1,10 @@
+// Copyright 2024 Yannic Staudt, tipi technologies Ltd and the goldilock contributors
+// SPDX-License-Identifier: GPL-2.0-only OR Proprietary
+#pragma once
+
+#include <cstddef>
+
+namespace tipi::goldilock::constants {
+  
+    inline const size_t CLI_DEFAULT_TIMEOUT_SECONDS = 60;
+}
